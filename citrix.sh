3eeebcc264eb5ac00c86408cb076d5e2bebb6d6a
Kyle Stubblefield <iamtuxmeister@gmail.com>
2023-08-17 18:00:23 -0700

    adding stuff

---
+++ citrix.sh
@@ -0,0 +1,2 @@
+wget -O icaclient.deb https://downloads.citrix.com/21993/icaclient_23.7.0.17_amd64.deb?__gda__=exp=1692323349~acl=/*~hmac=4a9a5972d56482574c0fc2e0641510fe4ff697c861e305ae4742c1fa8f3382e9
+wget -O ctxusb.deb https://downloads.citrix.com/21993/ctxusb_23.7.0.17_amd64.deb?__gda__=exp=1692323349~acl=/*~hmac=4a9a5972d56482574c0fc2e0641510fe4ff697c861e305ae4742c1fa8f3382e9
